7216b149a9ea0d37b16b6763f8ea73795ab6aede
dmitrii-vainman <dmitry.vainman@yahoo.com>
2025-03-06 10:33:07 +0100

    index angepasst

--- prop/src/App.jsx
+++ prop/src/App.jsx
@@ -2,7 +2,9 @@ function App() {
   return (
       <div>
           <h1>Willkommen zu meiner React-App!</h1>
+          <p>Ich pushe das lokal und es fliegt auf die ec2!</p>
       </div>
+      
   );
 }
 export default App;
