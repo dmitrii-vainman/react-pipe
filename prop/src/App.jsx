415eabe97362f36a9076b15c20a6a67abcd29691
dmitrii-vainman <dmitry.vainman@yahoo.com>
2025-03-06 10:43:21 +0100

    bug fix perm

--- prop/src/App.jsx
+++ prop/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
       <div>
           <h1>Willkommen zu meiner React-App!</h1>
-          <p>Ich pushe das lokal und es fliegt auf die ec2!!</p>
+          <p>Ich pushe das lokal und es fliegt auf die ec2!</p>
       </div>
       
   );
